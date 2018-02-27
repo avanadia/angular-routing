122baa83518ddd52a62b8a9b0487d9fedb05ac37
Anthony <nin0@nin0.com>
2018-02-26 22:48:24 -0500

    first commit

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  users: any[];
+  hasPermission: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
+    if (this.hasPermission) {
+      this.getUsers().then(users => this.users = users).catch(e => console.log(e.message));
+    } else {
+      this.users = [];
+    }
+  }
+
+  async getUsers() {
+    return [
+      {name: 'Anthony', email: 'anthony@sfl.com'},
+      {name: 'Sarah', email: 'sarah@osu.edu'}
+    ];
   }
 
 }
